c58daeb3d267fed8296490dc978449cfb5f16fa8
mujib ansati <mujib.uddin@froogal.ai>
2026-01-22 21:29:02 +0530

    removed dublicate

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2006,7 +2006,7 @@ const Home = (props) => {
         </div>
       </section> */}
 
-<section id="privacy-policy"  className="py-12 bg-gray-50">
+<section  className="py-12 bg-gray-50">
   <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 className="text-3xl font-bold text-gray-900 mb-8 text-center">Legal & Policies</h2>
     
